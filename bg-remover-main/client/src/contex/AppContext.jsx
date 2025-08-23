ecf1fc80d32fbccdf70337519db0afde58ed7412
namrata <namratajaiswal.716@gmail.com>
2025-08-23 15:18:43 +0530

    update 8

--- bg-remover-main/client/src/contex/AppContext.jsx
+++ bg-remover-main/client/src/contex/AppContext.jsx
@@ -11,7 +11,7 @@ const AppContextProvider = (props) => {
   const [image, setImage] = useState(false);
   const [resultImage, setResultImage] = useState(false);
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL || " ";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL || "";
   const navigate = useNavigate();
 
   const { getToken } = useAuth();
